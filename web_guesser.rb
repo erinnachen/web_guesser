6dcebf49af4a94bb3ea08d724574a76e2e7ec4d1
Erinna Chen <erinna.chen@gmail.com>
2016-01-23 13:15:40 -0700

    I4: Fun with colors implemented

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,17 +1,33 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'pry'
 
 set :SECRET_NUMBER, rand(100)
 
 get '/' do
   guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => {number: number, message: message}
+  color = get_color(guess)
+  erb :index, :locals => {number: number, message: message, color: color}
 end
 
 def number
   settings.SECRET_NUMBER
+end
+
+def get_color(guess)
+  return "White" unless guess
+  guess = guess.to_i
+  if guess > number + 5
+    "Red"
+  elsif guess > number
+    "Pink"
+  elsif guess < number - 5
+    "Blue"
+  elsif guess < number
+    "PowderBlue"
+  else
+    "LawnGreen"
+  end
 end
 
 def check_guess(guess)
