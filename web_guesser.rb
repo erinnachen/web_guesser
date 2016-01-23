3d1fc41e3dcba238cbb5c3be48dec9d351468c98
Erinna Chen <erinna.chen@gmail.com>
2016-01-23 12:53:42 -0700

    I3: number guessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,48 +2,30 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
-number = rand(100)
+set :SECRET_NUMBER, rand(100)
 
 get '/' do
   guess = params["guess"]
-  message = "Please guess a number:"
-  if guess
-    guess = guess.to_i
-    if guess > number + 5
-      message = "Way too high!"
-    elsif guess > number
-      message = "Too high!"
-    elsif guess < number - 5
-      message = "Way too low!"
-    elsif guess < number
-      message = "Too low!"
-    else
-      message = "You got it right!"
-    end
-  end
-
-  #message = check_guess(guess)
+  message = check_guess(guess)
   erb :index, :locals => {number: number, message: message}
 end
 
+def number
+  settings.SECRET_NUMBER
+end
 
-# def check_guess(guess)
-#   message = "Please guess a number:"
-#   return message unless guess
-#   guess = guess.to_i
-#   # if guess > @number
-#   #   if guess> @number + 5
-#   #     message = "Way too high!"
-#   #   else
-#   #     message = "Too high!"
-#   #   end
-#   # elsif guess < @number
-#   #   if guess < @number - 5
-#   #     message = "Way too low!"
-#   #   else
-#   #     message = "Too low!"
-#   #   end
-#   # else
-#   #   message = "You got it right!"
-#   # end
-# end
+def check_guess(guess)
+  return "Please guess a number:" unless guess
+  guess = guess.to_i
+  if guess > number + 5
+    "Way too high!"
+  elsif guess > number
+    "Too high!"
+  elsif guess < number - 5
+    "Way too low!"
+  elsif guess < number
+    "Too low!"
+  else
+    "You got it right!"
+  end
+end
